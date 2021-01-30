f7a16fbcb3358c36de53447ebab65c6484d16450
Renato Lima <renato.lima92@gmail.com>
2021-01-30 17:16:03 -0300

    baked for merge

--- Assets/Contents/Internal/Scripts/LoadingController.cs
+++ Assets/Contents/Internal/Scripts/LoadingController.cs
@@ -10,11 +10,37 @@ public class LoadingController : MonoBehaviour
     [SerializeField] private GameObject loadingUI;
     [SerializeField] private Image fadeUI;
 
+    private static LoadingController _instance;
+    public static LoadingController Instance => _instance;
+
+    private AsyncOperation ops;
+
     // Start is called before the first frame update
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-        SceneManager.sceneLoaded += OnSceneLoaded();
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded();
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    internal static void LoadAddictive(int index, Action<AsyncOperation> onLoad)
+    {
+        if(index == SceneManager.GetActiveScene().buildIndex)
+        {
+            Debug.LogError("fix buildindex");
+            return;
+        }
+        _instance.ops = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
+        _instance.ops.allowSceneActivation = false;
+        _instance.ops.completed += onLoad;
     }
 
     private UnityAction<Scene, LoadSceneMode> OnSceneLoaded()
@@ -41,6 +67,5 @@ public class LoadingController : MonoBehaviour
     private void FadeOut()
     {
         fadeUI.gameObject.SetActive(true);
-        Destroy(gameObject, 1);
     }
 }
