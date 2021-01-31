08511d6d209ecb4dd95dc5b48901b2c0cae30af7
Jeferson <jefersonblc@gmail.com>
2021-01-31 06:06:22 -0300

    Ajustes de movimento e Jump

--- Assets/Contents/Internal/Scripts/Mechanics/PlayerController.cs
+++ Assets/Contents/Internal/Scripts/Mechanics/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,27 +8,42 @@ namespace Mechanics
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
-        public int RunSpeed = 5;
+        public int RunSpeed = 2;
         public int DashPower = 2;
+        public int JumpImpulse = 10;
         public float TurnSmooth = 15f;
+        public float MaxSpeed = 3f;
+        public LayerMask GroundLayer;
 
         private InputMaster.GameplayControlsActions gameplayControls;
         private Rigidbody _body;
-
+        private Collider _collider;
         private Quaternion _targetRotation;
+        private bool _grounded;
 
         private void Awake()
         {
             gameplayControls = new InputMaster().GameplayControls;
             gameplayControls.Jump.performed += OnJumpPerformed;
             _body = GetComponent<Rigidbody>();
+            _collider = GetComponent<Collider>();       
         }
 
         private void OnJumpPerformed(InputAction.CallbackContext obj)
         {
-            Debug.Log("Jump!");
-            //TODO
+            if (_grounded)
+            {
+                Jump();
+            }
         }
+
+        private void Jump()
+        {
+            _body.AddForce(Vector3.up * JumpImpulse * 100, ForceMode.Impulse);
+            _grounded = false;
+        }
+
+
 
         private void OnEnable()
         {
@@ -44,6 +60,12 @@ namespace Mechanics
         {
             //Updates
             Movement(gameplayControls.Movement.ReadValue<Vector2>());
+            if (GroundDetected())
+            {
+                Landed();
+            };
+
+            LimitSpeed();
         }
 
         private void Movement(Vector2 move)
@@ -53,6 +75,27 @@ namespace Mechanics
             targetDir.y = 0f;
             HandleRotation(targetDir);
             HandleMovement(targetDir);
+        }
+
+        private bool GroundDetected()
+        {
+            return Physics.Raycast(_collider.bounds.center, Vector3.down, _collider.bounds.extents.y, GroundLayer.value);
+        }
+
+        private void Landed()
+        {
+            if (_grounded == false)
+            {
+                _grounded = true;
+            }
+        }
+
+        private void LimitSpeed()
+        {
+            if (_body.velocity.magnitude > MaxSpeed)
+            {
+                _body.velocity = _body.velocity.normalized * MaxSpeed;
+            }
         }
 
         private void HandleMovement(Vector3 targetDir)
@@ -65,7 +108,7 @@ namespace Mechanics
 
         private void HandleRotation(Vector3 targetDir)
         {
-            if (targetDir.magnitude > 0.1f)
+            if (targetDir.magnitude > 0.1f && _grounded)
             {
                 _targetRotation = Quaternion.LookRotation(targetDir, Vector3.up); 
                 Quaternion newRotation = Quaternion.Lerp(_body.rotation, _targetRotation, TurnSmooth * Time.deltaTime);
