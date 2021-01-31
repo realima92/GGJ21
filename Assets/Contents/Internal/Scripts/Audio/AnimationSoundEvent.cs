0d15cd549307e22070644b85bd474b8241fea9ac
Renato Lima <renato.lima92@gmail.com>
2021-01-30 23:59:17 -0300

    animation sound script update

--- Assets/Contents/Internal/Scripts/Audio/AnimationSoundEvent.cs
+++ Assets/Contents/Internal/Scripts/Audio/AnimationSoundEvent.cs
@@ -7,21 +7,8 @@ public class AnimationSoundEvent : MonoBehaviour
     public Transform audioParent;
     public List<StudioEventEmitter> emmiters;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void AudioStepEvent(int index)
     {
-        Debug.Log($"Opa! Animou legal. Bora tocar passos na posição {index}");
         emmiters[index].Play();
     }
 }
