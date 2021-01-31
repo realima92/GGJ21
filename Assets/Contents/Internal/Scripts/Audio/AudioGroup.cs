5036d4af4fb89da420ab36e82346c84347963606
Renato Lima <renato.lima92@gmail.com>
2021-01-31 10:14:42 -0300

    ui adjustments

---
+++ Assets/Contents/Internal/Scripts/Audio/AudioGroup.cs
@@ -0,0 +1,5 @@
+﻿public enum AudioGroup
+{
+    Music,
+    Sound
+}
---
+++ Assets/Contents/Internal/Scripts/Audio/AudioSettingsToggle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSettingsToggle : MonoBehaviour
+{
+    public AudioGroup group;
+
+    public Image icon;
+
+    public Sprite iconOn;
+    public Sprite iconOff;
+
+    private bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dataName = group.ToString() + "On";
+        isOn = PlayerPrefs.GetInt(dataName, 1) == 1;
+        icon.sprite = isOn ? iconOn : iconOff;
+    }
+
+    public void Toggle()
+    {
+        var dataName = group.ToString() + "On";
+        isOn = !isOn;
+        PlayerPrefs.SetInt(dataName, isOn ? 1 : 0);
+        icon.sprite = isOn ? iconOn : iconOff;
+    }
+}
